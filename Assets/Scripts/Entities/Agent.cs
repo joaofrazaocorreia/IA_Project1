3d41d13f594109ff0ae34caaa731ae6df40ce1d1
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-04 22:23:18 +0100

    comment agent class

---
+++ Assets/Scripts/Entities/Agent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Entities
+{
+    /// <summary>
+    /// Base, empty class. Defines an Agent.
+    /// </summary>
+    public abstract class Agent : MonoBehaviour
+    {
+
+    }
+}

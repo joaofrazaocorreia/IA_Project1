ad30d5fdc879fd0d81481f819886eb48376e12d3
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-04 22:22:33 +0100

    implement base class for unmovable agents

---
+++ Assets/Scripts/Entities/StaticAgent.cs
@@ -0,0 +1,17 @@
+﻿using LibGameAI.FSMs;
+
+namespace Entities
+{
+    /// <summary>
+    /// Defines an agent that doesn't move.
+    /// </summary>
+    public abstract class StaticAgent: Agent
+    {
+        protected StateMachine StateMachine;
+
+        private void Update()
+        {
+            StateMachine.Update();
+        }
+    }
+}

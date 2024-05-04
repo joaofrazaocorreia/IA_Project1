b1af47e256673cc89abcceda18286354d8620c1b
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-04 17:52:50 +0100

    flesh out destination class

--- Assets/Scripts/Locals/Destination.cs
+++ Assets/Scripts/Locals/Destination.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Entities;
+using TMPro;
 using UnityEngine;
 
 namespace Locals
 {
     public class Destination : MonoBehaviour
     {
+        public TMP_Text vehicleCounter;
+        public TMP_Text pedestrianCounter;
+        public Transform vehicleExitPoint;
+        public Transform pedestrianExitPoint;
+        
         private List<Agent> _agentsInside;
-
         private int _numberOfAgentsInside;
+        
+        public Vector3 position => transform.position;
+
+        private int NumberOfVehiclesInside() => _agentsInside.Count(x => x.GetType() == typeof(Vehicle));
+        private int NumberOfPedestriansInside() => _agentsInside.Count(x => x.GetType() == typeof(Pedestrian));
+
+        private void Update()
+        {
+            vehicleCounter.text = NumberOfVehiclesInside().ToString();
+            pedestrianCounter.text = NumberOfPedestriansInside().ToString();
+        }
 
         public void EnterDestination(Agent agent)
         {

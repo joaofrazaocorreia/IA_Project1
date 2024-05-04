80dac41ae1b4128b0b0f30a936b199b596abaa27
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-04 22:23:06 +0100

    add road waypoints + connectors

---
+++ Assets/Scripts/Locals/RoadConnector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Locals
+{
+    /// <summary>
+    /// Component that connects a road to another.
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class RoadConnector : MonoBehaviour
+    {
+        public RoadWaypoint waypointToConnect { get; set; }
+        
+        public RoadConnector[] otherConnections;
+        
+        /// <summary>
+        /// Gets a random connection connected to this one.
+        /// </summary>
+        public RoadConnector GetConnection()
+        {
+            return otherConnections.Length > 0 ? otherConnections[Random.Range(0, otherConnections.Length)] : null;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            
+            Gizmos.DrawSphere(transform.position - new Vector3(0, 
+                transform.localScale.y * 0.5f, 0), 0.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/Locals/RoadWaypoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Entities;
+using UnityEngine;
+
+namespace Locals
+{
+    /// <summary>
+    /// Defines a road point.
+    /// </summary>
+    public class RoadWaypoint: MonoBehaviour
+    {
+        public RoadConnector[] connections;
+        public int roadSpeedLimit = 20; //max speed vehicles can circulate at, on this road
+
+        private List<Vehicle> activeVehicles { get; set; }
+
+        private void Start()
+        {
+            activeVehicles = new List<Vehicle>();
+            
+            foreach (var r in connections)
+            {
+                r.waypointToConnect = this;
+            }
+        }
+
+        /// <summary>
+        /// Adds or removes a vehicle from the activeVehicles list.
+        /// </summary>
+        /// <param name="v"> The vehicle that is being added or removed from the list.</param>
+        /// <param name="add"> Whether or not the vehicle should be added to the list of active vehicles. 
+        /// If it is true, then it will be added. If false, then it will not be added and removed from the list
+        /// if already present.</param>
+        public void RegisterVehicle(Vehicle v, bool add)
+        {
+            if (add)
+            {
+                activeVehicles.Add(v);
+            }
+            else
+            {
+                if (activeVehicles.Contains(v))
+                {
+                    activeVehicles.Remove(v);
+                }
+            }
+        }
+    }
+}
